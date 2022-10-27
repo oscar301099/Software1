1119ab9edaf5f751b07e0250dae835ba9d646a8e
oscar oros <oscarorosduran@gmail.com>
2022-10-26 20:02:33 -0400

    primera subida

---
+++ database/migrations/2022_10_26_233434_create_proyecto_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyecto', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('descripcion');
+            $table->unsignedBigInteger('user_id')->foreign('users')->references('id');
+            $table->string('Contenido');
+            $table->string('Codigo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyecto');
+    }
+};
---
+++ database/migrations/2022_10_26_233532_create_proyecto_user_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyecto_user', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->foreign('users')->references('id');
+            $table->unsignedBigInteger('proyecto_id')->foreign('proyecto')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyecto_user');
+    }
+};
