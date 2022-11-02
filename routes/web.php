aaccdee55cb0ae1927bcd3a3b644875f11a551dd
oscar oros <oscarorosduran@gmail.com>
2022-11-02 16:08:40 -0400

    adminlte instalado

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
